e51462eb57e815e8398787b9ed45761c9f1aa083
Pratik Gupta <pratikguptamain@gmail.com>
2025-07-14 13:30:23 +0530

    installed imagekit and wrote auth config for it

---
+++ app/api/auth/imagekit-auth/route.ts
@@ -0,0 +1,26 @@
+import { getUploadAuthParams } from "@imagekit/next/server";
+
+export async function GET() {
+    try {
+        const authenticationParameters = getUploadAuthParams({
+        privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string,
+        publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY as string,
+        });
+    
+        return Response.json({
+            authenticationParameters,
+            publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
+        });
+    } catch (error) {
+        console.error("Imagekit auth failed", error);
+        
+        return Response.json(
+            {
+                error: "Authentication for Imagekit failed",
+            },
+            {
+                status: 500
+            }
+    );
+    }
+}
